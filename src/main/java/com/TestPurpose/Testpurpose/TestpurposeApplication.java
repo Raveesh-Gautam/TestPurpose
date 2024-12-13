7165275a9f712b6e3430d30b3852f2576318053c
Raveesh-Gautam <raveeshgautamfzd2002@gmail.com>
2024-12-14 01:14:23 +0530

    first

---
+++ src/main/java/com/TestPurpose/Testpurpose/TestpurposeApplication.java
@@ -0,0 +1,13 @@
+package com.TestPurpose.Testpurpose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestpurposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestpurposeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TestPurpose/Testpurpose/TestpurposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TestPurpose.Testpurpose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestpurposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
